72a0efb39647d3f20aa7cf967c7e29b54a13385f
hachiko <mertuygur02@gmail.com>
2020-12-31 00:33:36 +0300

    amongus part started

--- js/amongus.js
+++ js/amongus.js
@@ -1,19 +1,60 @@
 'use strict'
 
-var colorize = document.querySelectorAll('.box');
-const whiteboxes = Array.from(colorize)
+/* Selectors */
+
+var startbtn = document.querySelector('.start');
+var boxes1 = document.querySelectorAll('.box');
+var boxarray = Array.from(boxes1);
+
+var boxes2 = document.querySelectorAll('.boxt');
 
 
-
-console.log(whiteboxes)
-for (let i = 0; i < 1; i++) {
-    doIt();
+/* Functions */
+function getRandomArray(a, b) {
+    while (a.length < b) {
+        var r = Math.floor(Math.random() * 9);
+        if (a.indexOf(r) === -1) a.push(r);
+    }
 }
-function doIt() {
-    setInterval(function () {
-        var rand = Math.floor(Math.random() * colorize.length);
-        if (whiteboxes[rand].style.backgroundColor != 'red') {
-            whiteboxes[rand].style.backgroundColor = 'red';
+
+
+
+
+startbtn.addEventListener('click', () => {
+    /* first phase*/
+    var a = [];
+    var b = [];
+    getRandomArray(a, 4);
+    getRandomArray(b, 4);
+
+    for (let i = 0; i < 4; i++) {
+        boxes1[a[i]].classList.add('blacked');
+        setTimeout(() => {
+            boxes1[a[i]].classList.remove('blacked');
+        }, 1000)
+    }
+
+    setTimeout(() => {
+        for (let i = 0; i < 4; i++) {
+            boxes1[b[i]].classList.add('blacked');
+            setTimeout(() => {
+                boxes1[b[i]].classList.remove('blacked');
+            }, 1000)
         }
-    }, 1000)
-}
+    }, 2000)
+
+    /* second phase  */
+
+    for (const box of boxes2) {
+        box.addEventListener('click', () => {
+            box.classList.add('clickd');
+            setTimeout(() => {
+                box.classList.remove('clickd');
+            }, 300)
+        })
+    }
+
+
+
+})
+
